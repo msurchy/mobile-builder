2dfcc064c11cea328380ed354d010197ebfa65f5
Ngoc Dang <dangtienngoc@hotmail.com>
2020-08-25 11:05:07 +0700

    bump: 1.4.6

--- mobile-builder.php
+++ mobile-builder.php
@@ -30,7 +30,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MOBILE_BUILDER_CONTROL_VERSION', '1.4.7' );
+define( 'MOBILE_BUILDER_CONTROL_VERSION', '1.4.8' );
 
 define( 'MOBILE_BUILDER_PLUGIN_NAME', 'mobile-builder' );
 
